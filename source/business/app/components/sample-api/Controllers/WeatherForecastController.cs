8d08bdb3b2e997893942f10e6545aa71cf920ef5
Jacob-R96 <jacobrivera7271996@gmail.com>
2023-08-10 09:57:40 -0500

    sample-app.shared.csproj - updated to Net 6.0 from 5.0
    Sample-Api.Json - updated from Net50 to Net60
    
    Sample-App/Server/Controller/WeatherforcastController.cs -Client Local host port changed.
    
    Sample-Api/Controllers/WeatherforcastController.cs:
    Method inserted to display "Summaries" data.
    
    Sample-Api/Controllers/WeatherforcastController.cs:
     Array Method 'Summaries' commented out.

--- source/business/app/components/sample-api/Controllers/WeatherForecastController.cs
+++ source/business/app/components/sample-api/Controllers/WeatherForecastController.cs
@@ -15,7 +15,11 @@ namespace sample_api.Controllers
     [Route("[controller]")]
     public class WeatherForecastController : ControllerBase
     {
-        private static readonly string[] Summaries = new[]
+        //private static readonly string[] Summaries = new[]
+       // {
+          //  "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+       // };
+       private void Summaries(int temp)
         {
             "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
         };
